1203f8c712b522a581e509b579b7093e63cb45f4
Nikhil <mnnikh@amazon.com>
2018-09-09 21:04:54 -0700

    simple linear Regression

--- Part 1 - Data Preprocessing/data_preprocessing.py
+++ Part 1 - Data Preprocessing/data_preprocessing.py
@@ -34,3 +34,5 @@ from sklearn.preprocessing import StandardScaler
 sc_X = StandardScaler()
 X_train = sc_X.fit_transform(X_train)
 X_test = sc_X.transform(X_test)
+sc_y = StandardScaler()
+y_train = sc_y.fit_transform(y_train)
